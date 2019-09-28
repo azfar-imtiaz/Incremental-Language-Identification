Fix shape issues, remove softmax since we're using CrossEntropyLoss, get basic training loop working, use embeddings

GRUNet.py
@@ -4,43 +4,27 @@ import torch
 
 
 class GRUNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers, batch_size, dropout):
+    def __init__(self, vocab_size, seq_len, input_size, hidden_size, num_layers, output_size, dropout=0.0):
         super().__init__()
+        self.num_layers = 2
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.num_layers = num_layers
-        self.batch_size = batch_size
-        self.dropout = dropout
-
-        self.gru = nn.GRU(input_size=self.input_size, hidden_size=self.hidden_size,
-                          num_layers=self.num_layers, dropout=self.dropout, batch_first=False)
-        self.linear1 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.linear2 = nn.Linear(self.hidden_size, self.output_size)
-        self.softmax = nn.Softmax(dim=1)
+        self.emb = nn.Embedding(vocab_size, input_size)
+        self.gru = nn.GRU(input_size, hidden_size,
+                          num_layers=self.num_layers, dropout=dropout)
+        self.fc = nn.Linear(hidden_size * seq_len, output_size)
+        # self.softmax = nn.Softmax(dim=2)
 
     def forward(self, sequence):
-        hidden_state = self.init_hidden(len(sequence))
-        # print(hidden_state.type())
-        # print(sequence.type())
-        output, hidden = self.gru(sequence, hidden_state)
-        # output = output.contiguous().view(-1, len(sequence))
-        output = self.linear1(output)
-        output = F.relu(output)
-        output = self.linear2(output)
-        output = self.softmax(output)
+        output = self.emb(sequence)
+        hidden_layer = self.init_hidden(len(sequence[0]))
+        output, _ = self.gru(output, hidden_layer)
+        output = output.contiguous().view(
+            1, self.hidden_size * len(sequence[0]))
+        output = self.fc(output)
+        # don't need the softmax here as CrossEntropy loss already does softmax at its end
+        # output = self.softmax(output)
         return output
 
-    def init_hidden(self, sequence_length):
-        # return torch.Tensor([
-        #     torch.zeros(self.num_layers, sequence_length,
-        #                 self.hidden_size, dtype=torch.float),
-        #     torch.zeros(self.num_layers, sequence_length,
-        #                 self.hidden_size, dtype=torch.float),
-        #     torch.zeros(self.num_layers, sequence_length,
-        #                 self.hidden_size, dtype=torch.float)
-        # ])
-        # print("Hidden layer size: ")
-        # print(torch.randn([self.batch_size, sequence_length,
-        #                    self.hidden_size], dtype=torch.float).size())
-        return torch.randn([self.num_layers, self.batch_size, self.hidden_size], dtype=torch.float)
+    def init_hidden(self, seq_len):
+        return torch.zeros(self.num_layers, seq_len, self.hidden_size).float()

main.py
@@ -1,26 +1,29 @@
 import torch
-import numpy as np
 import torch.nn as nn
 from torch.optim import Adam
 from torch.nn.utils.rnn import pad_sequence
+import random
 
 import config
 from GRUNet import GRUNet
-from load_data import load_data, get_numeric_representations_sents, create_one_hot_vectors
+from load_data import load_data, get_numeric_representations_sents
 
 
 if __name__ == '__main__':
-    languages = ['eng', 'urd', 'fars']
+    languages = ['urd', 'fars', 'ara', 'srp', 'bos']
+    lang_label_to_int_mapping = {
+        lang_label: i for i, lang_label in enumerate(languages)
+    }
+
+    lang_int_to_label_mapping = {
+        i: lang_label for i, lang_label in enumerate(languages)
+    }
 
     print("Loading data...")
-    X, y = load_data(config.x_file, config.y_file, languages, clip_length=100,
-                     clip_sents=True, padding=True)
-    X = X[:5000]
-    y = y[:5000]
-    # for index in range(0, 5):
-    #     print(X[index])
-    #     print(y[index])
-    #     print("-" * 60)
+    X, y = load_data(config.x_file, config.y_file, languages,
+                     lang_label_to_int_mapping, clip_length=100, clip_sents=True, padding=True)
+    X = X[:500]
+    y = y[:500]
 
     print("Converting characters to numbers, generating vocabulary...")
     numeric_sequences, vocabulary = get_numeric_representations_sents(X)
@@ -30,25 +33,24 @@ if __name__ == '__main__':
 
     print("Padding sequences...")
     padded_sequences = pad_sequence(
-        numeric_sequences, batch_first=True)
-    for index in range(0, 5):
-        print(padded_sequences[index])
-        print(len(padded_sequences[index]))
+        numeric_sequences, batch_first=True, padding_value=0.0)
+    # for index in range(0, 5):
+    #     print(padded_sequences[index])
+    #     print(len(padded_sequences[index]))
 
-    print("Generating one hot representations...")
-    one_hot_vec_sequences = create_one_hot_vectors(
-        padded_sequences, vocabulary)
+    # print("Generating one hot representations...")
+    # one_hot_vec_sequences = create_one_hot_vectors(
+    #     padded_sequences, vocabulary)
     # for index in range(len(one_hot_vec_sequences)):
     #     print(one_hot_vec_sequences[index])
     #     print(one_hot_vec_sequences[index].eq(1).sum().item())
 
-    # print(one_hot_vec_sequences.size())
-    print(len(y))
-
     # initializing the network
     print("Initializing the network...")
-    model = GRUNet(input_size=len(one_hot_vec_sequences[0]), hidden_size=300,
-                   output_size=3, num_layers=2, batch_size=config.BATCH_SIZE, dropout=0.0)
+    model = GRUNet(vocab_size=len(vocabulary) + 1, seq_len=len(
+        padded_sequences[0]), input_size=200, hidden_size=300,
+        output_size=len(languages), num_layers=2, dropout=0.2)
+
     print(model)
     criterion = nn.CrossEntropyLoss()
     learning_rate = config.LEARNING_RATE
@@ -58,26 +60,19 @@ if __name__ == '__main__':
     model.train()
     for epoch in range(config.NUM_EPOCHS):
         print("Epoch: %d" % (epoch + 1))
-        model.zero_grad()
-        for index, sequence in enumerate(one_hot_vec_sequences):
-            try:
-                sequences_batch = [one_hot_vec_sequences[i]
-                                   for i in range(index, index + config.BATCH_SIZE)]
-                outputs_batch = [y[i]
-                                 for i in range(index, index + config.BATCH_SIZE)]
-            except IndexError as e:
-                print("We have reached the end of the dataset, break!")
-                print(str(e))
-                break
+        zipped_data = list(zip(padded_sequences, y))
+        random.shuffle(zipped_data)
+        padded_sequences, y = zip(*zipped_data)
+        epoch_loss = 0.0
+        # for index, sequence in enumerate(one_hot_vec_sequences):
+        for index, (input_seq, output_seq) in enumerate(zip(padded_sequences, y)):
             optimizer.zero_grad()
-            # output = model(torch.FloatTensor([[sequence]]))
-            # print(np.asarray([sequences_batch]).shape)
-            output = model(torch.FloatTensor([sequences_batch]))
-            # print("Output size: {}".format(output.size()))
-            # print("Label size: {}".format(np.asarray(outputs_batch).shape))
-            loss = criterion(output, torch.Tensor([outputs_batch]).long())
+            output = model(torch.stack([input_seq]).long())
+            loss = criterion(output, torch.LongTensor([output_seq]))
             loss.backward()
             optimizer.step()
 
-            if index % 500 == 0 and index > 0:
-                print(loss.item())
+            # if index % 500 == 0 and index > 0:
+            #     print(loss.item())
+            epoch_loss += loss.item()
+        print("Loss at epoch {}: {}".format(epoch + 1, epoch_loss))