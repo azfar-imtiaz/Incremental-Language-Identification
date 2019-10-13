5e1918ee24da87805c5ada43a119cc91bdd67464
Muhammad Azfar Imtiaz <gusimtmu@GU.GU.SE@mltgpu.flov.gu.se>
2019-10-13 16:38:18 +0200

    Bug fixes + latest model and losses plot

--- test_model.py
+++ test_model.py
@@ -39,8 +39,8 @@ def test_model(model, vocab_mapping, lang_int_to_label_mapping, X_test, Y_test, 
                               ]['total_predictions'] += 1
             input = torch.stack([padded_seq]).long()
             input = input.to(dev)
-            hidden_layer = model.init_hidden(padded_seq.size(1))
-            output = model(input, hidden_layer)
+            hidden_layer = model.init_hidden(len(padded_seq))
+            output, _ = model(input, hidden_layer)
             _, prediction = torch.max(output.data, dim=1)
             if prediction == test_label:
                 correct_predictions_per_instance += 1
